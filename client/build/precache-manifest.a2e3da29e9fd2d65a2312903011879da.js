a912faf8bb80285d7fb45530d0a68b66055c4d0b
cfpaige <cfpaige@gmail.com>
2019-08-04 08:51:09 -0700

    fixing paths

---
+++ client/build/precache-manifest.a2e3da29e9fd2d65a2312903011879da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "97b9d33608a9fad11d14",
+    "url": "/static/js/main.6d7eead8.chunk.js"
+  },
+  {
+    "revision": "2b8afee3b8b573165fd7",
+    "url": "/static/js/2.4cfcba0e.chunk.js"
+  },
+  {
+    "revision": "97b9d33608a9fad11d14",
+    "url": "/static/css/main.c51c7aa1.chunk.css"
+  },
+  {
+    "revision": "2cd97b53e9844cdb5e6dcd83fbe4b5e6",
+    "url": "/index.html"
+  }
+];
